e598cbfe39597a422767d5f07ab10af6f5e66df5
Linet Oliech <linetaoliech@gmail.com>
2022-12-06 06:38:56 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
+    let id = document.getElementById('searchByID');
+    let form = document.querySelector('form');
+    form.addEventListener('submit', function(e){
+      e.preventDefault()
+    fetch(`http://localhost:3000/movies/${id.value}`)
+    .then(response => response.json())
+    .then(data => {
+      const title = document.querySelector('section#movieDetails h4');
+      const summary = document.querySelector('section#movieDetails p');
+      title.innerHTML=data.title
+      summary.innerHTML=data.summary
+    });
+    })
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
